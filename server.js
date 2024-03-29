39721c8bb452bb903e6a921486bee3bbd4b67fce
omar <omaralshafei02@gmail.com>
2024-03-29 18:02:16 -0400

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('frontend/bugit ild'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
